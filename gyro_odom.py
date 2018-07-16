de1d41181a7649851dd55b36f7fa25d373b87eb7
Leon Wehmeier <lw96@st-andrews.ac.uk>
2018-07-16 09:57:44 +0100

    fixed start up sequence

--- gyro_odom.py
+++ gyro_odom.py
@@ -24,19 +24,19 @@ def my_callback(event):
     pub.publish(Vector3(x=0, y=0, z=abs_z))
     odom = Odometry()
     odom.header.stamp = rospy.Time.now()
-    odom.header.frame_id = "gyro_odom"
+    odom.header.frame_id = "base_footprint"
     # since all odometry is 6DOF we'll need a quaternion created from yaw
     odom_quat = tf.transformations.quaternion_from_euler(0, 0, abs_z)
     #odom_quat[3]=1.0
     odom.pose.pose = Pose(Point(0., 0., 0.), Quaternion(*odom_quat))
-    odom.child_frame_id = "base_link"
+    odom.child_frame_id = "odom"
     odom.twist.twist = Twist(Vector3(0, 0, 0), Vector3( 0, 0, rel_z))
     odom_pub.publish(odom)
 
 rospy.init_node('gyro_odom')
-rospy.Timer(rospy.Duration(0.1), my_callback)
 odom_pub = rospy.Publisher("/gyro/odom", Odometry, queue_size=3)
 pub = rospy.Publisher("/gyro/sum", Vector3, queue_size=3)
 rospy.Subscriber("/gyro/z", Float32, callbackZ)
+rospy.Timer(rospy.Duration(0.1), my_callback)
 r = rospy.Rate(10) # 10hz
 rospy.spin()
